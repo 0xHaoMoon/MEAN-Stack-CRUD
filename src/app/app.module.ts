bbd7785885a3b1cba8b48668d55696f7284a68dc
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-10-29 21:47:46 +0100

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +17,14 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     PostCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
