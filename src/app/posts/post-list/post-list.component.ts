8828463d2d68475c371304d5936a167eedb6d2bc
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-11-01 11:56:20 +0100

    add subscribe

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Post } from "../post.models";
 import { PostsService } from '../posts.service';
 
@@ -8,14 +8,22 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit{
+export class PostListComponent implements OnInit, OnDestroy{
   panelOpenState = false;
   posts:Post[] = [];
-
+  private postsSub: Subscription;
 
     constructor(public postsService: PostsService) { }
 
   ngOnInit(): void {
     this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostsUpdateListener()
+    .subscribe((posts:Post[])=>{
+      this.posts = posts;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.postsSub.unsubscribe();
   }
 }
