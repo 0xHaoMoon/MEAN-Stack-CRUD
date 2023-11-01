64096679474ac7e22725ece452e79707e4fa430d
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-11-01 15:09:34 +0100

    try with dummy array

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('First middleware');
-    // next udamit es nicht auf req wartet 
-    next();
-});
-
-app.use((req, res, next)=>{
-    res.send('Hello from express')
+app.use('/api/posts',(req, res, next)=>{
+    const posts=[
+        {id: 'asd234234', title:'First Tiele', content:'first content'},
+        {id: 'asda456456345', title:'Second Tiele', content:'second content'},
+        {id: 'sgjhrghr5646', title:'Third Tiele', content:'third content'}
+    ]
+    res.status(200).json({
+        message: 'posts fetched succesfully',
+        posts: posts
+    })
 });
 
 
