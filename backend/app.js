a2a40474687795e1af9ab29eb45cb65e41a6ead6
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-11-02 08:24:24 +0100

    mongoose connect

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require ('mongoose')
+
+const Post = require('./models/post');
+
 const app = express();
+
+mongoose.connect('mongodb+srv://haotruong:demo@tutorial.epiyt3a.mongodb.net/node-angular?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('Connected to database!');
+})
+.catch(()=>{
+    console.log('Connected to database failed!');
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
@@ -19,7 +31,11 @@ app.use((req, res, next) => {
   });
 
   app.post('/api/posts',(req, res, next)=>{
-    const post = req.body;
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    post.save();
     console.log(post)
     res.status(201).json({
         message: 'Post added succesfully'
@@ -30,7 +46,8 @@ app.use('/api/posts',(req, res, next)=>{
     const posts=[
         {id: 'asd234234', title:'First Tiele', content:'first content'},
         {id: 'asda456456345', title:'Second Tiele', content:'second content'},
-        {id: 'sgjhrghr5646', title:'Third Tiele', content:'third content'}
+        {id: 'sgjhrghr5646', title:'Third Tiele', content:'third content'},
+        {"_id":{"$oid":"65434323c449a39ba7bedd15"},"title":"asdasd","content":"asdasdasd","__v":{"$numberInt":"0"}}
     ]
     res.status(200).json({
         message: 'posts fetched succesfully',
